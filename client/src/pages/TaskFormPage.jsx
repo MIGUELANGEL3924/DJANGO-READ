d35e65a768f4cb3be0765a91fdddec907d3ca9d7
MIGUELANGEL3924 <mkitito2484@gmail.com>
2024-09-12 20:53:39 -0500

    modificando TaskFormPage

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,3 +1,27 @@
+import { useForm } from "react-hook-form";
+
 export function TaskFormPage() {
-  return <div>TaskFormPage</div>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="title"
+          {...register("title", { required: true })}
+        />
+        <textarea
+          rows="3"
+          placeholder="Description"
+          {...register("description", { required: true })}
+        ></textarea>
+        <button>Save</button>
+      </form>
+    </div>
+  );
 }
