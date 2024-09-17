991f4d73cf08860321ccfb29450eed12b67d0bf4
MIGUELANGEL3924 <mkitito2484@gmail.com>
2024-09-17 17:15:35 -0500

    agregando get y post en tasks

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,10 +1,18 @@
 import { useForm } from "react-hook-form";
+import { createTask } from "../api/tasks.api";
+import { useNavigate } from "react-router-dom";
 
 export function TaskFormPage() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
 
-  const onSubmit = handleSubmit((data) => {
-    console.log(data);
+  const onSubmit = handleSubmit(async (data) => {
+    await createTask(data);
+    navigate("/tasks");
   });
 
   return (
@@ -15,11 +23,15 @@ export function TaskFormPage() {
           placeholder="title"
           {...register("title", { required: true })}
         />
+        {errors.title && <span>title is required</span>}
+
         <textarea
           rows="3"
           placeholder="Description"
           {...register("description", { required: true })}
         ></textarea>
+        {errors.description && <span>description is required</span>}
+
         <button>Save</button>
       </form>
     </div>
