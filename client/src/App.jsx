58150fc10f183a7a8374e2a8327c4f9c38c9dc29
MIGUELANGEL3924 <mkitito2484@gmail.com>
2024-09-21 20:02:30 -0500

    agregando delete tasks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { TasksPage } from "./pages/TasksPage";
 import { TaskFormPage } from "./pages/TaskFormPage";
 import { Navigation } from "./components/Navigation";
@@ -8,8 +8,10 @@ function App() {
     <BrowserRouter>
       <Navigation />
       <Routes>
-        <Route path="/task" element={<TasksPage />} />
-        <Route path="/task-create" element={<TaskFormPage />} />
+        <Route path="/" element={<Navigate to="/tasks" />} />
+        <Route path="/tasks" element={<TasksPage />} />
+        <Route path="/tasks-create" element={<TaskFormPage />} />
+        <Route path="/tasks/:id" element={<TaskFormPage />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -7,4 +7,6 @@ const tasksApi = axios.create({
 export const getAllTasks = () => tasksApi.get("/");
     
 export const createTask=(task) => tasksApi.post("/", task);
+
+export const deleteTask = (id) => tasksApi.delete('/${id}')
   
